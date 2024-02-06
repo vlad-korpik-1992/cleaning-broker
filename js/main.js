eb6f9b1d0e4351ac9698117c6afc9456292f60bf
Vlad <v.korpik2015@gmail.com>
2024-02-06 10:42:54 +0300

    The layout of the project is complete

--- js/main.js
+++ js/main.js
@@ -152,3 +152,165 @@ submit.onclick = (e) => {
       }
     )
 }
+
+let questionAnswer = '';
+
+function nextSliderResidental(event) {
+  event.preventDefault();
+  const slider = document.querySelector('.quiz__box--first');
+  const sliderSecond = document.querySelector('.quiz__box--second');
+  slider.classList.remove('quiz__box--active');
+  sliderSecond.classList.add('quiz__box--active');
+  questionAnswer = questionAnswer + 'Are you interested in Residential or Cleaning: RESIDENTIAL<br/>';
+}
+
+function nextSliderCommercial(event) {
+  event.preventDefault();
+  const slider = document.querySelector('.quiz__box--first');
+  const sliderSecond = document.querySelector('.quiz__box--second');
+  slider.classList.remove('quiz__box--active');
+  sliderSecond.classList.add('quiz__box--active');
+  questionAnswer = questionAnswer + 'Are you interested in Residential or Cleaning: COMMERCIAL<br/>';
+}
+
+function nextSliderThird(event) {
+  event.preventDefault();
+  const slider = document.querySelector('.quiz__box--second');
+  const sliderNext = document.querySelector('.quiz__box--third');
+  let checkboxes = document.getElementsByName('answer1[]');
+  let answers = '';
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked === true) {
+      answers = answers + checkboxes[i].value + "|";
+    }
+  }
+  if (answers != '') {
+    slider.classList.remove('quiz__box--active');
+    sliderNext.classList.add('quiz__box--active');
+    questionAnswer = questionAnswer + ' What services are you currently interested in: ' + answers + '<br/>';
+  }
+}
+
+function nextSliderFour(event) {
+  event.preventDefault();
+  let answers = '';
+  const slider = document.querySelector('.quiz__box--third');
+  const sliderNext = document.querySelector('.quiz__box--four');
+  const square = document.querySelector('[name="square"]').value;
+  const bedrooms = document.querySelector('[name="bedrooms"]').value;
+  const bathrooms = document.querySelector('[name="bathrooms"]').value;
+  const floors = document.querySelector('[name="floors"]').value;
+  let checkboxes = document.getElementsByName('answer3[]');
+  let date = document.querySelector('[name="date"]').value;
+  data = new FormData();
+  const uploadField = document.getElementById("files");
+  if (uploadField.files.length >= 6) {
+    document.getElementById('files__status').textContent = "It is forbidden to upload more than 5 files";
+    return false;
+  }
+  if (uploadField.files.length >= 1) {
+    for (let i = 0; i < uploadField.files.length; i++) {
+      if (uploadField.files[i].size > 10048576) {
+        document.getElementById('files__status').textContent = "The file size should not exceed 10MB";
+        return false;
+      }
+      if (/\.(jpe?g|png|PNG|gif|tif)$/i.test(uploadField.files[i].name) === false) {
+        document.getElementById('files__status').textContent = "Invalid file format";
+        return false;
+      }
+      data.append('files[]', document.getElementById('files').files[i]);
+    }
+  }
+  questionAnswer = questionAnswer + ' Square footage: ' + square + '<br/>';
+  questionAnswer = questionAnswer + ' Bedrooms: ' + bedrooms + '<br/>';
+  questionAnswer = questionAnswer + ' Bathrooms: ' + bathrooms + '<br/>';
+  questionAnswer = questionAnswer + ' Exterior floors: ' + floors + '<br/>';
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked === true) {
+      answers = answers + checkboxes[i].value + "|";
+    }
+  }
+  questionAnswer = questionAnswer + ' Options: ' + answers + '<br/>';
+  questionAnswer = questionAnswer + ' Desired date of cleaning: ' + date + '<br/>';
+  data.append('answer', questionAnswer);
+  slider.classList.remove('quiz__box--active');
+  sliderNext.classList.add('quiz__box--active');
+}
+
+function nextSliderFive(event) {
+  event.preventDefault();
+  let answers = '';
+  const slider = document.querySelector('.quiz__box--four');
+  const sliderNext = document.querySelector('.quiz__box--five');
+  const message = document.querySelector('[name="message"]').value;
+  questionAnswer = questionAnswer + 'Its there anything else you would like us to know?: ' + message + '<br/>';
+  data.append('answerFive', questionAnswer);
+  slider.classList.remove('quiz__box--active');
+  sliderNext.classList.add('quiz__box--active');
+}
+
+/* Ajax Quiz Send*/
+$(document).ready(function () {
+  $('#quiz-send').click(function (e) {
+    e.preventDefault();
+    x = document.querySelector('[name="name-quiz"').value;
+    console.log(x);
+    if (x === "") {
+      document.querySelector('.quiz__form__error').textContent = "Please enter your name";
+      return false;
+    }
+    x = document.querySelector('[name="address-quiz"').value;
+    if (x === "") {
+      document.querySelector('.quiz__form__error').textContent = "Please provide your address";
+      return false;
+    }
+    x = document.querySelector('[name="phone-quiz"').value;
+    if (x === "") {
+      document.querySelector('.quiz__form__error').textContent = "Please give me your phone number";
+      return false;
+    }
+    x = document.querySelector('[name="email-quiz"').value;
+    if (x === "") {
+      document.querySelector('.quiz__form__error').textContent = "Please enter your E-mail";
+      return false;
+    } else {
+      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(x)) {
+        document.querySelector('.quiz__form__error').textContent = "Incorrect E-mail";
+        return false;
+      }
+    }
+    let checkboxes = document.getElementsByName('answer5[]');
+    let answers = '';
+    for (let i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked === true) {
+        answers = answers + checkboxes[i].value + "|";
+      }
+    }
+    questionAnswer = questionAnswer + ' What is your preferred way of contact? ' + answers + '<br/>';
+    $('#quiz__form__error').removeClass("quiz__form__error--warning");
+    document.getElementById('quiz__form__error').textContent = "There's a shipment coming in...";
+    data.append('firstname', $('input[name=name-quiz]').val());
+    data.append('address', $('input[name=address-quiz]').val());
+    data.append('phone', $('input[name=phone-quiz]').val());
+    data.append('email', $('input[name=email-quiz]').val());
+    data.append('answerFinish', questionAnswer);
+    $.ajax({
+      url: './quiz-mail.php',
+      type: "POST",
+      data: data,
+      processData: false,
+      contentType: false,
+      success: function () {
+        $('#quiz__inner__items--grid').trigger('reset');
+        $('#quiz__form__error').text("The message has been successfully sent. An expert will contact you in a few minutes");
+        setTimeout(function () {
+          $('#quiz__form__error').text("").addClass("error");
+        }, 2000);
+      },
+      error: function (jqXHR) {
+        $('#quiz__form__error').text(jqXHR);
+      }
+    });
+  });
+});
